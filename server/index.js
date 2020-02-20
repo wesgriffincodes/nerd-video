1203e2ca7d9734506902555788a5d7870574c024
codingclueless <codingclueless@gmail.com>
2020-02-20 13:35:23 -0800

    k

--- server/index.js
+++ server/index.js
@@ -9,9 +9,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(pino);
 
-var distDir = __dirname + "/dist/";
- app.use(express.static(distDir));
-
 const sendTokenResponse = (token, res) => {
   res.set('Content-Type', 'application/json');
   res.send(
